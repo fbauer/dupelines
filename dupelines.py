64515137378c1cae57128d4905b568de669f1eb7
flo <flo@62a0fb6f-3b2f-0410-b377-dbd0903a6fe7>
2007-04-29 08:11:54 +0000

    added help message and command line options
    git-svn-id: file:///home/flo/development/dupelines_repository/dupelines@2 62a0fb6f-3b2f-0410-b377-dbd0903a6fe7

--- dupelines.py
+++ dupelines.py
@@ -4,37 +4,62 @@ import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.pylab import plot, show
 import sys
+import optparse
 
-def show_similiarities(filename):
-    f = open(filename, 'r')
-    #lines = f.readlines()
-    lines = f.read()
-    x_positions = []
-    y_positions = []
-    lines_x = []
-    lines_y = []
-    possible_line_continuations = {}
-    for xPos, first_line in enumerate(lines[:-1]):
-        for yPos, second_line in enumerate(lines[xPos+1:]):
-            if first_line == second_line and first_line != '\n':
-                if (xPos, yPos) in possible_line_continuations:
-                    possible_line_continuations[xPos + 1, yPos + 1] =\
-                    possible_line_continuations.pop((xPos, yPos))
-                else:
-                    possible_line_continuations[xPos + 1, yPos + 1] = (xPos, yPos)
-                x_positions.append(xPos)
-                y_positions.append(yPos)
-    
-    for after_line_end in possible_line_continuations.iterkeys():
-        start = possible_line_continuations[after_line_end]
-        stop = (after_line_end[0]-1, after_line_end[1]-1)
-        if start != stop:
-            lines_x.extend(range(start[0] - 1, after_line_end[0]))
-            lines_y.extend(range(start[1] - 1, after_line_end[1]))
+def show_similiarities(filenames):
+    for filename in filenames:
+        f = open(filename, 'r')
+        lines = f.readlines()
+        #lines = f.read()
+        x_positions = []
+        y_positions = []
+        lines_x = []
+        lines_y = []
+        possible_line_continuations = {}
+        for xPos, first_line in enumerate(lines[:-1]):
+            for yPos, second_line in enumerate(lines[xPos+1:]):
+                if first_line == second_line and first_line != '\n':
+                    if (xPos, yPos) in possible_line_continuations:
+                        possible_line_continuations[xPos + 1, yPos + 1] =\
+                        possible_line_continuations.pop((xPos, yPos))
+                    else:
+                        possible_line_continuations[xPos + 1, yPos + 1] = (xPos, yPos)
+                    x_positions.append(xPos)
+                    y_positions.append(yPos)
+        
+        for after_line_end in possible_line_continuations.iterkeys():
+            start = possible_line_continuations[after_line_end]
+            stop = (after_line_end[0]-1, after_line_end[1]-1)
+            if start != stop:
+                lines_x.extend(range(start[0] - 1, after_line_end[0]))
+                lines_y.extend(range(start[1] - 1, after_line_end[1]))
     plot(x_positions, y_positions, 'b.')
     plot(lines_x, lines_y, 'ro')
     show()
+
 if __name__ == '__main__':
-    show_similiarities(sys.argv[1])
+    
+
+    from optparse import OptionParser
+    usage = ("usage: %prog [options] filename [filenames]\n" +
+             "Show duplicated lines in filename.\n" + 
+             "If more than one file is specified, show duplicates " +
+             "in all of them")
+    parser = OptionParser(usage=usage)
+    available_parsers = ['text', 'python']
+    parser.add_option("-p", "--parser", dest="parse_with", type='choice',
+                      choices=available_parsers,
+                      help="specify parser to use\n%s" % available_parsers, metavar='PARSER')
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="print additional informations to stderr")
+
+    (options, args) = parser.parse_args()
+    if not args:
+        parser.print_help()
+        sys.exit(2)
+    show_similiarities(args)
+# vim: ts=4,et,sw=4
+
     
 
